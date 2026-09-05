1c6b0183e72cdce83289a8655b71ec2a5aed03fe
Aabhas Parande <aabhasp6@gmail.com>
2026-09-05 20:03:08 +0100

    update marketEventSimulator

--- src/MatchingEngine.cpp
+++ src/MatchingEngine.cpp
@@ -31,32 +31,35 @@ void MatchingEngine :: MatchOrder(Order& order, std::map<double, std::queue<Orde
         if (!type.empty()){
             if (order.type == OrderType::LIMIT ){
                 if (side == Side::BUY && ob.getAsk().begin()->first > price_){
-                    Order new_order(id,OrderType::LIMIT,Side::BUY,price_,remaining);
+                    new_order = Order(id,OrderType::LIMIT,Side::BUY,price_,remaining);
                     ob.addOrder(new_order);
                     break;
                 }
                 if (side == Side::SELL && ob.getBid().begin()->first < price_ ){
-                    Order new_order(id,OrderType::LIMIT,Side::SELL,price_,remaining);
+                    new_order = Order(id,OrderType::LIMIT,Side::SELL,price_,remaining);
                     ob.addOrder(new_order);
                     break;
                 }
             }
         }
-        std::queue<Order>& queue = type.begin()->second;
-        if (remaining >= queue.front().quantity){
-            if (side == Side::BUY){
-                Trade trade(queue.front().price,id,queue.front().id,queue.front().quantity);
-                store_trades.push_back(trade);    
-            }
-            else{
-                Trade trade(queue.front().price,queue.front().id,id,queue.front().quantity);
-                store_trades.push_back(trade);
-            }
 
-            remaining -= queue.front().quantity;
-            queue.pop();
+        std::queue<Order>& queue = type.begin()->second;
+
+        Trade trade;
+        if (side == Side::BUY){
+            trade = Trade(queue.front().price,id,queue.front().id,queue.front().quantity);    
         }
         else{
+            trade = Trade(queue.front().price,queue.front().id,id,queue.front().quantity);
+        }
+
+        if (remaining >= queue.front().quantity){
+            remaining -= queue.front().quantity;
+            queue.pop();
+            store_trades.push_back(trade);
+        }
+        else{
+            store_trades.push_back(trade);
             queue.front().quantity -= remaining;
             remaining = 0;
             break;
